9f65cf601833ac9fd82f38c5b0e074ba53c9b5f4
João Henrique <jenryk9503@gmail.com>
2019-12-28 11:19:25 -0300

    Updates

---
+++ precache-manifest.1921cc0106e68351006b06ff138a11b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350103b3da05543af82e0adf7d241e0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d908d522acf36244975",
+    "url": "/static/css/main.4907c755.chunk.css"
+  },
+  {
+    "revision": "7ec0d14c3f609ae771b9",
+    "url": "/static/js/2.3a5caef0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3a5caef0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d908d522acf36244975",
+    "url": "/static/js/main.fd09f95e.chunk.js"
+  },
+  {
+    "revision": "7a796a8307f45d35e5d2",
+    "url": "/static/js/runtime-main.8f7319eb.js"
+  },
+  {
+    "revision": "a009920790306d0b969482232c8ee223",
+    "url": "/static/media/lego.a0099207.jpg"
+  }
+]);
